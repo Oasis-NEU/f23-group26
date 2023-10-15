b5e6cda7c7069a0ab5a0388ca978eaa84cf8357c
leakwok <lea.a.kwok@gmail.com>
2023-10-15 13:01:59 -0400

    initializing project

---
+++ fren/fren/frenApp.swift
@@ -0,0 +1,17 @@
+//
+//  frenApp.swift
+//  fren
+//
+//  Created by Lea Kwok on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct frenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
